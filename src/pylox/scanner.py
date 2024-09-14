8942ea838ffa2672dc8b2ae91ef74665ca058c03
Taneli <taneli.leppanen@hotmail.com>
2024-09-14 19:22:03 +0300

    The scanner class 4.4

--- src/pylox/scanner.py
+++ src/pylox/scanner.py
@@ -1,4 +1,4 @@
-from collections.abc import Generator
+import typing
 from enum import Enum, auto
 
 
@@ -52,16 +52,42 @@ class TokenType(Enum):
     EOF = auto()
 
 
+type LiteralValue = typing.Any
+
+
 class Token:
-    def __init__(self) -> None:
-        pass
+    __slots__ = ("type", "lexeme", "literal", "line")
+
+    def __init__(self, type: TokenType, lexeme: str, literal: LiteralValue, line: int) -> None:
+        self.type = type
+        self.lexeme = lexeme
+        self.literal = literal
+        self.line = line
+
+    def __str__(self) -> str:
+        return f"{self.type} {self.lexeme} {self.literal}"
 
 
 class Scanner:
-    __slots__ = "source"
+    __slots__ = ("source", "tokens", "start", "current", "line")
 
     def __init__(self, source: str) -> None:
         self.source = source
 
-    def scan_tokens(self) -> Generator[Token, None, None]:
-        yield Token()
+        self.tokens: list[Token] = []
+        self.start = 0
+        self.current = 0
+        self.line = 1
+
+    def scan_single_token(self):
+        raise NotImplementedError()
+
+    def scan_tokens(self) -> list[Token]:
+        while not self.is_at_end():
+            self.start = self.current
+            self.scan_single_token()
+        self.tokens.append(Token(TokenType.EOF, "", None, self.line))
+        return self.tokens
+
+    def is_at_end(self) -> bool:
+        return self.current >= len(self.source)
