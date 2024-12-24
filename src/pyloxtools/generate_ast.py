789619471c74cf38e24789a941b068d5eb15073b
Tzane <taneli.leppanen@hotmail.com>
2024-12-24 22:37:25 +0200

    Add visitor to generate_ast

--- src/pyloxtools/generate_ast.py
+++ src/pyloxtools/generate_ast.py
@@ -1,74 +1,100 @@
 import argparse
-import sys
 from pathlib import Path
-from typing import TYPE_CHECKING
-
-if TYPE_CHECKING:
-    from _typeshed import SupportsWrite
+from io import TextIOWrapper
 
 
-def generate_ast(output_dir: Path | None) -> None:
-    expr_types = [
-        "Binary   : Expr left, Token operator, Expr right",
-        "Grouping : Expr expression",
-        "Literal  : Any value",
-        "Unary    : Token operator, Expr right",
-    ]
-    base_name = "Expr"
-    if output_dir is None:
-        define_ast(
-            sys.stdout,
-            base_name,
-            expr_types,
-        )
-    else:
-        with open(output_dir.joinpath("expr.py"), "w") as fs:
-            define_ast(
-                fs,
-                base_name,
-                expr_types,
-            )
+class WriteLn:
+    def __init__(self, file: TextIOWrapper):
+        self.file = file
+
+    def writeln(self, line: str) -> int:
+        return self.file.write(line + "\n")
 
 
-def define_ast(file: "SupportsWrite[str]", base_name: str, types: list[str]) -> None:
-    # Imports
-    print("from abc import ABC", file=file)
-    print("from typing import Any", file=file)
-    print("", file=file)
-    print("from pylox.scanner import Token", file=file)
-    print("\n", file=file)
-    # Base Class
-    print(f"class {base_name}(ABC):", file=file)
-    print("    pass", file=file)
-    print("", file=file)
-
-    # AST classes
-    for t in types:
-        print("", file=file)
-        class_name, fields = map(lambda s: s.strip(), t.split(":"))
-        define_type(file, base_name, class_name, fields)
+def generate_ast(output_dir: Path) -> None:
+    define_ast(
+        output_dir,
+        "Expr",
+        [
+            "Binary   : Expr left, Token operator, Expr right",
+            "Grouping : Expr expression",
+            "Literal  : Any value",
+            "Unary    : Token operator, Expr right",
+        ],
+    )
 
 
-def define_type(file: "SupportsWrite[str]", base_name: str, class_name: str, fields: str) -> None:
+def define_ast(output_dir: Path, base_name: str, types: list[str]) -> None:
+    with open(output_dir.joinpath(f"{base_name.lower()}.py"), "w") as fs:
+        file = WriteLn(fs)
+        define_imports(file)
+        define_base_class(file, base_name)
+
+        # AST classes
+        for t in types:
+            file.writeln("")
+            class_name, fields = map(lambda s: s.strip(), t.split(":"))
+            define_type(file, base_name, class_name, fields)
+            file.writeln("")
+
+        file.writeln("")
+        define_visitor(file, base_name, types)
+
+
+def define_imports(file: WriteLn) -> None:
+    file.writeln("from abc import ABC, abstractmethod")
+    file.writeln("from typing import Any")
+    file.writeln("")
+    file.writeln("from pylox.scanner import Token")
+    file.writeln("\n")
+
+
+def define_base_class(file: WriteLn, base_name: str) -> None:
+    file.writeln(f"class {base_name}(ABC):")
+    file.writeln("    @abstractmethod")
+    file.writeln(f'    def accept(self, visitor: "{base_name}Visitor"):  # noqa: U100')
+    file.writeln("        raise NotImplementedError()")
+    file.writeln("")
+
+
+def define_type(file: WriteLn, base_name: str, class_name: str, fields: str) -> None:
     # (type, name)
     variables = [tuple(i.split(" ")) for i in fields.split(", ")]
-    print(f"class {class_name}({base_name}):", file=file)
+    file.writeln(f"class {class_name}({base_name}):")
     slots = ", ".join(f'"{var_name}"' for (_, var_name) in variables)
-    print(f"    __slots___ = ({slots})", file=file)
-    print("", file=file)
+    if len(variables) == 1:
+        slots += ","
+    file.writeln(f"    __slots___ = ({slots})")
+    file.writeln("")
     init_header = ", ".join(f"{va_name}: {var_type}" for (var_type, va_name) in variables)
-    print(f"    def __init__(self, {init_header}) -> None:", file=file)
-    print("        super().__init__()", file=file)
+    file.writeln(f"    def __init__(self, {init_header}) -> None:")
+    file.writeln("        super().__init__()")
+    # Fields
     for _, i_n in variables:
-        print(f"        self.{i_n} = {i_n}", file=file)
-    print("", file=file)
+        file.writeln(f"        self.{i_n} = {i_n}")
+    file.writeln("")
+    file.writeln(f'    def accept(self, visitor: "{base_name}Visitor"):')
+    file.writeln(f"        return visitor.visit_{class_name.lower()}_{base_name.lower()}(self)")
+
+
+def define_visitor(file: WriteLn, base_name: str, types: list[str]) -> None:
+    file.file.write(f"class {base_name}Visitor(ABC):")
+    base_lower = base_name.lower()
+
+    for t in types:
+        file.writeln("")
+        type_name = t.split(":")[0].strip()
+        file.writeln("    @abstractmethod")
+        file.writeln(
+            f"    def visit_{type_name.lower()}_{base_lower}(self, {base_lower}: {type_name}):"
+            "  # noqa: U100"
+        )
+        file.writeln("        raise NotImplementedError()")
 
 
 def cli() -> None:
     parser = argparse.ArgumentParser("generate_ast")
-    parser.add_argument(
-        "output_dir", type=lambda i: Path(i) if i is not None else None, default=None, nargs="?"
-    )
+    parser.add_argument("output_dir", type=Path)
     parsed_args = parser.parse_args()
 
     generate_ast(parsed_args.output_dir)
